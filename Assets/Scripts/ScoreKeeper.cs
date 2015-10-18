3721d6135ac371020df5d8bd6e9392f0e6b111aa
Peter Mauldin <pmauldin@snaptrends.com>
2015-10-18 14:11:02 -0500

    Added score

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -12,8 +12,21 @@ public class ScoreKeeper : MonoBehaviour
 	public GameObject HeroAboutToWinSound;
 	public GameObject GameWonSound;
 	public float[] TimeTillChangeSprite;
+	private static int score1 = 0;
+	private static int score2 = 0;
+	private static int score3 = 0;
+	private static int score4 = 0;
 	private float ChangeTime;
+	private float x_displacement = Screen.width/(7.2f); 
+	private float y_displacement = (Screen.height/6);
+	private GUIStyle guiStyleRoundCount = new GUIStyle();
 	
+	void Start()
+	{
+		guiStyleRoundCount.fontSize = Screen.width/18;
+		guiStyleRoundCount.alignment = TextAnchor.UpperCenter;
+	}
+
 	Hero FindWinningPlayer()
 	{
 		List<Hero> livingPlayers = new List<Hero>();
@@ -51,6 +64,10 @@ public class ScoreKeeper : MonoBehaviour
 		SoundFX.Instance.StopMusic();
 		ChangeTime = Time.time + TimeTillChangeSprite[index - 1];
 		Hero.CurrentWinnerIndex = index;
+		if(index == 1) score1++;
+		if(index == 2) score2++;
+		if(index == 3) score3++;
+		if(index == 4) score4++;
 	}
 	
 	void StopVictorySound()
@@ -72,6 +89,28 @@ public class ScoreKeeper : MonoBehaviour
 		if (hero != null) {
 			this.WinningHero = hero;
 			this.PlayVictorySound(this.WinningHero, this.WinningHero.PlayerIndex);
+		}
+	}
+
+	void OnGUI()
+	{
+		if(this.WinningHero != null) {
+			int i = 0;
+			GUI.Label(new Rect(115 + (i * x_displacement), 20, (i * x_displacement) + 115 + 50, 60), "Player 1", guiStyleRoundCount);
+			GUI.Label(new Rect(115 + (i * x_displacement), 100, (i * x_displacement) + 115 + 50, 140), score1.ToString(), guiStyleRoundCount);
+			
+			i = 1;
+			GUI.Label(new Rect(115 + (i * x_displacement), 20, (i * x_displacement) + 115 + 50, 60), "Player 2", guiStyleRoundCount);
+			GUI.Label(new Rect(115 + (i * x_displacement), 100, (i * x_displacement) + 115 + 50, 140), score2.ToString(), guiStyleRoundCount);
+			
+			i = 2;
+			GUI.Label(new Rect(115 + (i * x_displacement), 20, (i * x_displacement) + 115 + 50, 60), "Player 3", guiStyleRoundCount);
+			GUI.Label(new Rect(115 + (i * x_displacement), 100, (i * x_displacement) + 115 + 50, 140), score3.ToString(), guiStyleRoundCount);
+			
+			i = 3;
+			GUI.Label(new Rect(115 + (i * x_displacement), 20, (i * x_displacement) + 115 + 50, 60), "Player 4", guiStyleRoundCount);
+			GUI.Label(new Rect(115 + (i * x_displacement), 100, (i * x_displacement) + 115 + 50, 140), score4.ToString(), guiStyleRoundCount);
+		
 		}
 	}
 	
