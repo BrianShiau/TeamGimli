f22f621d3cbf55e29c8950d742fcce56a184ab28
BrianShiau <brian-s@utexas.edu>
2015-10-18 00:20:39 -0500

    added correct victory testing

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -11,42 +11,65 @@ public class ScoreKeeper : MonoBehaviour
 	public Hero HeroAboutToWin;
 	public GameObject HeroAboutToWinSound;
 	public GameObject GameWonSound;
-
+	
 	Hero FindWinningPlayer()
 	{
+		List<Hero> livingPlayers = new List<Hero>();
+		
+		Hero[] heroes = FindObjectsOfType(typeof(Hero)) as Hero[];
+		foreach (Hero hero in heroes) {
+			if (hero.IsAlive) {
+				livingPlayers.Add(hero);
+			}
+		}
+		if (livingPlayers.Count == 1)
+		{
+			return livingPlayers[0];
+		}
+		
 		return null;
 	}
-
+	
 	void StartHeroAboutToWinSound(Hero hero)
 	{
 		SoundFX.Instance.StopMusic();
 		this.HeroAboutToWinSound = SoundFX.Instance.OnHeroAboutToWin(hero);
 	}
-
+	
 	void StopHeroAboutToWinSound()
 	{
 		SoundFX.Instance.StartMusic();
 		AudioSourceExt.StopClipOnObject(this.HeroAboutToWinSound);
 		Destroy(this.HeroAboutToWinSound);
 	}
-
+	
 	void PlayVictorySound(Hero hero)
 	{
 		this.GameWonSound = SoundFX.Instance.OnMatchWon(hero);
 		SoundFX.Instance.StopMusic();
 	}
-
+	
 	void StopVictorySound()
 	{
 		AudioSourceExt.StopClipOnObject(this.GameWonSound);
 		Destroy(this.GameWonSound);
 	}
-
+	
 	void Update()
 	{
 		
+		if (this.WinningHero != null)
+		{
+			return;
+		}
+		
+		Hero hero = this.FindWinningPlayer();
+		if (hero != null) {
+			this.WinningHero = hero;
+			this.PlayVictorySound(this.WinningHero);
+		}
 	}
-
+	
 	public void ResetGame()
 	{
 		this.StopHeroAboutToWinSound();
@@ -54,7 +77,7 @@ public class ScoreKeeper : MonoBehaviour
 		SoundFX.Instance.StartMusic();
 		this.WinningHero = null;
 		this.HeroAboutToWin = null;
-
+		
 		Hero[] heroes = FindObjectsOfType(typeof(Hero)) as Hero[];
 	}
 }
