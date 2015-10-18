845f2920a76633efef8ce87eff646812f7aa67da
Adam Leslie <adamhleslie@gmail.com>
2015-10-18 14:08:55 -0500

    blood spatters different depending on who died, and they are on the default layer now (very hacky fix)

--- Assets/Scripts/DestroyAfter.cs
+++ Assets/Scripts/DestroyAfter.cs
@@ -6,12 +6,13 @@ public class DestroyAfter : MonoBehaviour
 {
 	public float Duration = 5.0f;
 
+    void Start()
+    {
+        this.GetComponent<SpriteRenderer>().sortingLayerName = "Default";
+    }
+
 	void Update()
 	{
-		this.Duration -= Time.deltaTime;
-		if (this.Duration < 0.0f)
-		{
-			GameObject.Destroy (this.gameObject);
-		}
+
 	}
 }
