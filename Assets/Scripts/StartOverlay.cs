9144f662ff8de86c5abb06a0947e866bc43ab435
Adam Leslie <adamhleslie@gmail.com>
2015-10-18 12:55:31 -0500

    Round Count added to overlay

--- Assets/Scripts/StartOverlay.cs
+++ Assets/Scripts/StartOverlay.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class StartOverlay : MonoBehaviour {
 
+    private static int RoundCount = 0;
     private GUIStyle guiStyle = new GUIStyle();
-    private float x_displacement = (-1) * (Screen.width/3.5f);
-    private float x_increment = Screen.width/4;
-    private float y_displacement = (-1) * (Screen.height/2);
+    private GUIStyle guiStyleRoundCount = new GUIStyle();
+    private float x_displacement = (-1) * (Screen.width/8);
+    private float x_increment = Screen.width/8;
+    private float y_displacement = (Screen.height/6);
     private int start_num = 3;
     public float time_per_num = 1.2f;
     private int cur_num;
@@ -14,19 +16,24 @@ public class StartOverlay : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        RoundCount++;
         cur_num = start_num;
         Time.timeScale = 0;
-        guiStyle.fontSize = Screen.width/2;
-        guiStyle.alignment = TextAnchor.UpperCenter;
+        guiStyleRoundCount.fontSize = Screen.width/5;
+        guiStyleRoundCount.alignment = TextAnchor.UpperCenter;
+        guiStyle.fontSize = Screen.width/3;
+        guiStyle.alignment = TextAnchor.MiddleCenter;
         endTime = Time.realtimeSinceStartup + time_per_num;
 	}
 	
-    void OnGUI()
-    {
+    void OnGUI() {
+        GUI.Label(new Rect(0, 0, Screen.width,Screen.height), "Round " + RoundCount, guiStyleRoundCount);
+   
+       // GUI.Label(new Rect(Screen.width/2.2f, Screen.height/2 + y_displacement,100,100), "Round " + RoundCount, guiStyleRoundCount);
         if(cur_num == 0)
-            GUI.Label(new Rect(Screen.width/2.2f, Screen.height/2 + y_displacement,100,100), "GO", guiStyle);
+            GUI.Label(new Rect(0, y_displacement, Screen.width,Screen.height), "GO", guiStyle);
         else
-            GUI.Label(new Rect(Screen.width/2 + x_displacement, Screen.height/2 + y_displacement,100,100), cur_num + "", guiStyle);
+            GUI.Label(new Rect(x_displacement, y_displacement, Screen.width + x_displacement,Screen.height), cur_num + "", guiStyle);
     }
 
 	// Update is called once per frame
